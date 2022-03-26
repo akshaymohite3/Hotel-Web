648afc46c2fb5b8abd1f64cede6b0558144a802d
Akshay Mohite <mohiteakshay069@gmail.com>
2022-03-26 20:08:07 +0530

    fies added

---
+++ index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous"/>
+</head>
+<body>
+<div id="first_page">
+<div class="container">	
+
+	<nav class="navbar navbar-expand">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="binary">
+  	      <h3>Binary Hat</h3>
+        </div>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="row" id="nav_content">
+                <a class="nav-link" href="#">ABOUT</a>
+                <a class="nav-link" href="#">SCREENSHOT</a>
+                <a class="nav-link" href="#">CONTACT</a>
+                <a class="nav-link call" href="#">Call Us:<span class="nav_num">12345677890</span> </a>
+              </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="row">
+    	<div class="col-sm-6" id="main_content">
+        <div>
+    		<h1>Healthy<br>Food<br>Recipes</h1>
+    		<p>There are many variation of passages of Lorem Ipsum <br>available,but the majorityomised word's which don't look even<br>slightly believable.</p>
+    		<button id="readmore">Read More</button>
+        </div>
+    	</div>
+    	<div class="col-sm-6">
+    		<div class="card">
+          <ul class="progress-bar">
+             <li>Name</li>
+             <li>Email</li>
+             <li>Phone number</li>
+             <li>Message</li>
+          </ul>
+          <div>
+          	<button id="btnsend">SEND</button>
+          </div>
+                
+        </div>
+    	</div>
+    </div>
+</div>
+</div>
+
+<div class="container" id="foodpackage">
+	<div class="contentbox">
+      <h2><span class="foodclr">Food</span> <span class="packegeclr">Packages</span></h2>
+      <p>There are many variation of passages of Lorem Ipsum available,but the majorityomised words which don't look<br>even slightly believable</p>
+
+	    <div  class="content">
+		    <div class="servics">
+		    	<div class="homemade"><img src="food1.png" alt="avatar" class="rounded-circle-me-n2" id="image1" ></div>
+          <div class="egg">
+			    <h3>Homemade</h3>
+			    <p>There are many variation of passages <br> of Lorem ipsum <br>available,but the majority have <br>suffered alternation in some form, <br>by injected humour,or <br>randomised words which don't <br>look even slightly belivable. </p>
+         </div>
+		    </div>
+		    <div class="servics">
+          <div class="homemade"><img src="food2.png" alt="avatar" class="rounded-circle-me-n2" id="image1" ></div>
+          <div class="egg">
+			    <h3>Noodles</h3>
+			    <p>There are many variation of passages <br> of Lorem ipsum <br>available,but the majority have <br>suffered alternation in some form, <br>by injected humour,or <br>randomised words which don't <br>look even slightly belivable. </p>
+        </div>
+		    </div>
+		    <div class="servics">
+          <div class="homemade"><img src="food3.png" alt="avatar" class="rounded-circle-me-n2" id="image1" ></div>
+          <div class="egg">
+			    <h3>Egg</h3>
+			    <p>There are many variation of passages <br> of Lorem ipsum <br>available,but the majority have <br>suffered alternation in some form, <br>by injected humour,or <br>randomised words which don't <br>look even slightly belivable. </p>
+		    </div>
+    		</div>
+    	</div>
+  </div>
+</div>
+
+<div class="container">
+	<div class="contentbox">
+		<div>How it works</div>
+		<h2><span class="step_clr">3 Step For </span><span class="step_clr1">Healthy Eating</span></h2>
+		<div class="main">
+
+		    <div class="content2">
+          <div class="corner-circle1">01</div>
+			    <p>There are many variation of passages <br> of Lorem ipsum <br>available,but the majority have <br>suffered alternation in some form, <br>by injected humour,or <br>randomised words which don't <br>look even slightly belivable. </p>
+		    </div>
+		    <div class="content2">
+          <div class="corner-circle2">02</div>
+			    <p>There are many variation of passages <br> of Lorem ipsum <br>available,but the majority have <br>suffered alternation in some form, <br>by injected humour,or <br>randomised words which don't <br>look even slightly belivable. </p>
+		    </div>
+		    <div class="content2">
+          <div class="corner-circle3">03</div>
+			    <p>There are many variation of passages <br> of Lorem ipsum <br>available,but the majority have <br>suffered alternation in some form, <br>by injected humour,or <br>randomised words which don't <br>look even slightly belivable. </p>
+		    </div>
+	    </div>
+</div>
+</div>
+<div  id="client-says">
+<div class="container">
+    <div class="row">
+        
+      <div class="col-sm-6 cli_content">
+        <h3><span class="step_clr">Clients</span><span class="say"> says</span></h3>
+        <div class="mark_du">  
+          <div><img src="client.png" class="client_img"></div>
+          <div class="mark_p">
+            <p><span class="mark_clr">MARK DU</span><br> It is lon established fact that a reader will be distracted by the <br>by readable content of a page when looking at its layout.The point of <br>using lorem ipsum is that it has a more-or-less normal distribution <br>of letters.</p>
+            <img src="toy_img.png">
+          </div>
+        </div>
+        <div class="circle">
+          <i class="fa-solid fa-circle"></i>
+          <i class="fa-solid fa-circle"></i>
+          <i class="fa-solid fa-circle"></i>
+        </div>
+      </div>
+      <div class="col-sm-6 food_margin">
+          <img src="food4.png" class="food4_img1" style="width: 100%;">
+        </div>
+   </div>
+</div>
+</div>
+<div class="container ">
+  <div class="end_content">
+    <h3>Lorem ipsum is simply dummy <br>text of the printing</h3>
+    <div class="end_p"><p>where does it comes from?</p></div>
+    <button class="get_btn">Get A Quote</button>
+  </div>
+</div>
+<div class="container-fluid">
+  <div class="copyright">
+    <p>Copyright 2022 All Right Reserved.</p>
+  </div>
+</div>    
+</body>
+</html>
